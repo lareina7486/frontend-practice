c535f8267c14f86778211bd7ab4cbc2291a191a5
lareina7486 <lareina7486@gmail.com>
2026-02-10 22:47:25 +0900

    feat: 컴포넌트 스타일링

--- react-practice/src/components/Main.jsx
+++ react-practice/src/components/Main.jsx
@@ -4,21 +4,26 @@
 // 객체는 {obj} 불가능, {obj.a} 가능!
 // 3. 모든 태그는 닫혀있어야 함.
 // 4. 최상위 태그는 반드시 1개 (fragment 사용 가능)
+// 5. CSS는 카멜케이스, className 사용
+
+import './Main.css';
 
 const Main = () => {
   const name = 'esther';
   const point = 30;
   const number = 19;
+  const isLogin = true;
 
   return (
     <>
       <h1>Main</h1>
       <h2>안녕하세요 {name}님!</h2>
-      <h3>현재 포인트는 {point + 5}점 입니다.</h3>
+      <h3 className="point">현재 포인트는 {point + 5}점 입니다.</h3>
       <h3>
         {name}님의 순서는 {number}({number % 2 == 0 ? '짝수' : '홀수'})번째
         입니다.
       </h3>
+      <h3>{isLogin ? <div>로그아웃</div> : <div>로그인</div>}</h3>
     </>
   );
 };
