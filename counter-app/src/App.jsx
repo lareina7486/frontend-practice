51005946b8844145d68a64c98e6c23fbe8d8891a
lareina7486 <lareina7486@gmail.com>
2026-02-12 23:57:17 +0900

    feat: App 컴포넌트에서 state 값 관리

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
 import View from './components/View';
 import Buttons from './components/Buttons';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <>
       <h1>Simple Counter</h1>
-      <View />
-      <Buttons />
+      <View count={count} />
+      <Buttons onClick={onClickButton} />
     </>
   );
 }
