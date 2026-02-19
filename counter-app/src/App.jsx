272c55c48af23f4bce8a02b33476b8ab8268334d
lareina7486 <lareina7486@gmail.com>
2026-02-19 22:21:52 +0900

    feat: useEffect를 추가하여 count 값 변경 시 로그 출력

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css';
 import View from './components/View';
 import Buttons from './components/Buttons';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    console.log(`count: ${count}`);
+  }, [count]); // 의존성 배열 (deps, dependency array)
+
   const onClickButton = (value) => {
     setCount((prev) => prev + value);
+    // console.log(count); // 변경되기 이전의 count 값이 출력됨 (setCount가 비동기이기 때문)
   };
 
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>Simple Counter</h1>
       <View count={count} />
       <Buttons onClickButton={onClickButton} />
